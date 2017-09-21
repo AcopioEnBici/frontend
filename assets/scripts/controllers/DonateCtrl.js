4b658e2a28144a8983233ba01207b559c3d7d22f
vicoysito <im.vicoy@gmail.com>
2017-09-21 02:43:34 -0500

    Implementacion de mapa y drag en donaciones

--- assets/scripts/controllers/DonateCtrl.js
+++ assets/scripts/controllers/DonateCtrl.js
@@ -1,3 +1,9 @@
+/*
+Victor Espinosa:im.vicoy@gmail.com
+twitter:@vicoysito
+*/
+
+
 'use strict';
 
 angular.module('app')
@@ -9,20 +15,33 @@ angular.module('app')
         "NgMap",
         function($rootScope, $scope, errAlertS, successAlertS, NgMap) {
             var initiated = false;
+            $scope.map;
             var root = firebase.database().ref("/");
             $scope.donator = {};
 
             var init = function() {
                 initiated = true;
-                NgMap.getMap().then(function(map) {
-                  console.log(map.getCenter());
-                  console.log('markers', map.markers);
-                  console.log('shapes', map.shapes);
-                });
+
+                  //Victor:Para inicializar el mapa se tiene que poner el ID que se tiene en el template
+                  NgMap.getMap("map").then(function(evtMap){
+                    $scope.map = evtMap;
+                  });
+
+            }
+
+            //Victor: funcion para obtener longitud y latitud
+            $scope.getGrabPosition = function(){
+              var position = $scope.map.markers[0].getPosition();
+              return {
+                "lat": position.lat(),
+                "long":position.lng()
+              }
             }
 
             $scope.save = function(){
-                console.log('saving', $scope.donator);
+
+              console.info('position', $scope.getGrabPosition());
+              console.log('saving', $scope.donator);
                 $scope.donator.createdAt = moment().valueOf();
                 $scope.donator.status = 'esperando';
                 root.child('donations').push($scope.donator).then(function(){
